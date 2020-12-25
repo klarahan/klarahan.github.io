f3c0061894baa61e7a92263d950ebfb1e57ccb1a
klarahan <hanoul@gmail.com>
2020-12-26 01:39:59 +0900

    clean up tokenizing functions

--- preprocess.R
+++ preprocess.R
@@ -20,33 +20,33 @@ library(stringr)
 
 path <- ("C:/Users/hanou/Dropbox/0 MYCOMP/0_Book manuscript/Lexington/data/all_issues.xls")
 
-chos_wel <- read_xls(path, sheet = "Chosun_ë³µì?€", col_types = "text") %>%  
+chos_wel <- read_xls(path, sheet = "Chosun_º¹Áö", col_types = "text") %>%  
   mutate(Newspaper = "Chosun", Keyword = "welfare") 
-chos_uni <- read_xls(path, sheet = "Chosun_?†µ?¼", col_types = "text") %>%  
+chos_uni <- read_xls(path, sheet = "Chosun_ÅëÀÏ", col_types = "text") %>%  
   mutate(Newspaper = "Chosun", Keyword = "unification") 
-chos_eco <- read_xls(path, sheet = "Chosun_ê²½ì œë¯¼ì£¼?™”", col_types = "text") %>%  
+chos_eco <- read_xls(path, sheet = "Chosun_°æÁ¦¹ÎÁÖÈ­", col_types = "text") %>%  
   mutate(Newspaper = "Chosun", Keyword = "econdem") 
 
 data_chos <- rbind(chos_wel, chos_uni, chos_eco) %>% 
   mutate(iidx = row_number(),
          Date = as.double(Date))
 
-hani_wel <- read_xls(path, sheet = "Hani_ë³µì?€", col_types = "text") %>%  
+hani_wel <- read_xls(path, sheet = "Hani_º¹Áö", col_types = "text") %>%  
   mutate(Newspaper = "Hankyoreh", Keyword = "welfare") 
-hani_uni <- read_xls(path, sheet = "Hani_?†µ?¼", col_types = "text") %>%  
+hani_uni <- read_xls(path, sheet = "Hani_ÅëÀÏ", col_types = "text") %>%  
   mutate(Newspaper = "Hankyoreh", Keyword = "unification") 
-hani_eco <- read_xls(path, sheet = "Hani_ê²½ì œë¯¼ì£¼?™”", col_types = "text") %>%  
+hani_eco <- read_xls(path, sheet = "Hani_°æÁ¦¹ÎÁÖÈ­", col_types = "text") %>%  
   mutate(Newspaper = "Hankyoreh", Keyword = "econdem") 
 
 data_hani <- rbind(hani_wel, hani_uni, hani_eco) %>% 
   mutate(iidx = row_number(),
          Date = as.double(Date))
 
-hank_wel <- read_xls(path, sheet = "Hangook_ë³µì?€", col_types = "text") %>%  
+hank_wel <- read_xls(path, sheet = "Hangook_º¹Áö", col_types = "text") %>%  
   mutate(Newspaper = "Hankook", Keyword = "welfare") 
-hank_uni <- read_xls(path, sheet = "Hangook_?†µ?¼", col_types = "text") %>%  
+hank_uni <- read_xls(path, sheet = "Hangook_ÅëÀÏ", col_types = "text") %>%  
   mutate(Newspaper = "Hankook", Keyword = "unification") 
-hank_eco <- read_xls(path, sheet = "Hangook_ê²½ì œë¯¼ì£¼?™”", col_types = "text") %>%  
+hank_eco <- read_xls(path, sheet = "Hangook_°æÁ¦¹ÎÁÖÈ­", col_types = "text") %>%  
   mutate(Newspaper = "Hankook", Keyword = "econdem") 
 
 data_hank <- rbind(hank_wel, hank_uni, hank_eco) %>% 
@@ -167,7 +167,7 @@ data_econ <- readRDS("data_econ.RDS")
 data_ideo <- readRDS("data_ideo.RDS")
 data_dem <- readRDS("data_dem.RDS")
 
-# 3a. Pre-processing ---------------------------------------------------------------------
+# 3. Pre-processing ---------------------------------------------------------------------
 data <- readRDS("data.RDS")
 Sys.setlocale("LC_ALL", locale = "korean")
 setwd("C:/Users/hanou/Dropbox/RESEARCH/klarahan.github.io")
@@ -176,41 +176,10 @@ library(dplyr, warn.conflicts = FALSE)
 # Suppress summarise info
 options(dplyr.summarise.inform = FALSE)
 
-#Plan A: the below method takes an exponential amount of time
-# data_kor <- data %>%
-#   mutate(cleaned = gsub("[^?„±-?…|?…-?…£|ê°€-?£]", " ", Body, perl = TRUE)) %>%
-#   unnest_tokens(word, cleaned, token = "words") %>%
-#   mutate(word = str_squish(word))
-# 
-# data_kor_noun <- data_kor %>%
-#   mutate(word = iconv(word, from = "utf-8", "cp949")) %>%
-#   unnest_tokens(noun, word, token = nouns) %>%
-#   filter(nchar(noun) > 1)             #filter words shorter than 1 syllable
-# data_kor_noun$noun
-# 
-# saveRDS(data_kor_noun, "data_kor_noun.RDS")
-
-#noun extracting function
-# extract_nouns <- function(x, keyword = keyword) {
-#   data_econ <- x %>%
-#     filter(Keyword == keyword) %>% 
-#     slice_sample(n = 1500) %>% 
-#     mutate(cleaned = gsub("[^?„±-?…|?…-?…£|ê°€-?£]", " ", Body, perl = TRUE)) %>%
-#     unnest_tokens(word, cleaned, token = "words") %>%
-#     mutate(word = str_squish(word)) %>% 
-#     mutate(word = iconv(word, from = "utf-8", "cp949")) %>%
-#     unnest_tokens(noun, word, token = nouns) %>% 
-#     filter(nchar(noun) > 1) %>% 
-#     group_by(Date, Title, Body, Newspaper, Keyword, Government, Prezparty) %>% 
-#     summarize(text = str_c(noun, collapse = " ")) %>%
-#     ungroup() 
-# }
-# y <- extract_nouns(data, "econdem")
-
 #Plan B: the below method is fast
 extract_nouns2 <- function(x) {
   data_econ <- x %>%
-    mutate(cleaned = gsub("[^?„±-?…|?…-?…£|ê°€-?£]", " ", Body, perl = TRUE)) %>%
+    mutate(cleaned = gsub("[^°¡-ÆR]", " ", Body, perl = TRUE)) %>%
     unnest_tokens(word, cleaned, token = "words") %>%
     mutate(word = str_squish(word)) %>% 
     mutate(word = iconv(word, from = "utf-8", "cp949")) %>%
@@ -318,85 +287,3 @@ toks_wel <- readRDS("data/data_wel_nouns") %>%
 saveRDS(toks_wel, "data/toks_wel")
   
   
-# 3b. Quanteda approach -------------------------------------------------------
-
-# create corpus with the library "quanteda"
-# corpus <- corpus(data, text_field = "Body")
-
-# # tokenize corpus and apply pre-processing
-# toks_ <- corpus %>% tokens(remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>% #general cleaning
-#   tokens_remove(pattern = stopwords("ko", source = "marimo"), min_nchar = 2) %>% #stopwords, min 2 syllables
-#   tokens_keep(pattern = "^\\p{script=Hangul}+$", valuetype = 'regex') %>%          #keep only Hangul
-#   tokens_remove(c("ì¢…ì´?‹ ë¬¸ë³´ê¸?", "(ê¸°ì|?ˆ˜?„?…¼?„¤?œ„?›|?Š¹?ŒŒ?›)", "?•œêµ??•„?´?‹·ì»? ë¬´ë‹¨? „?¬ ë°? ?¬ë°°í¬ ê¸ˆì?€", 
-#                   "?¸?„°?„·?•œêµ??¼ë³? ë¬´ë‹¨ ? „?¬ ë°? ?¬ë°°í¬ ê¸ˆì?€", "ì§€ë©´PDFë³´ê¸°", "ê´€? ¨ê¸°ì‚¬",   
-#                   "?•œêµ??•„?´?‹·ì»?|?•œêµ??˜¨?¼?¸?‹ ë¬¸í˜‘?šŒ|?””ì§€?„¸?‰´?Š¤?´?š©ê·œì¹™?— ?”°ë¥? ??€?‘ê¶Œì„ ?–‰?‚¬?•©?‹ˆ?‹¤"),
-#                 valuetype = "glob")
-# # print(toks[2], max_ndoc = 1, max_ntok = -1)
-# # 
-# # toks <- toks_ %>%
-# #   tokens_remove(c("ì¢…ì´?‹ ë¬¸ë³´ê¸?", "(ê¸°ì|?ˆ˜?„?…¼?„¤?œ„?›|?Š¹?ŒŒ?›)", "?•œêµ??•„?´?‹·ì»? ë¬´ë‹¨? „?¬ ë°? ?¬ë°°í¬ ê¸ˆì?€", 
-# #                   "?¸?„°?„·?•œêµ??¼ë³? ë¬´ë‹¨ ? „?¬ ë°? ?¬ë°°í¬ ê¸ˆì?€", "ì§€ë©´PDFë³´ê¸°", "ê´€? ¨ê¸°ì‚¬",   
-# #                   "?•œêµ??•„?´?‹·ì»?|?•œêµ??˜¨?¼?¸?‹ ë¬¸í˜‘?šŒ|?””ì§€?„¸?‰´?Š¤?´?š©ê·œì¹™?— ?”°ë¥? ??€?‘ê¶Œì„ ?–‰?‚¬?•©?‹ˆ?‹¤",
-# #                   "?ˆ?Š”", "??€?•œ", "ë§í–ˆ?‹¤", "?´?¼ê³?", "ê¸°ì", "ê·¸ë˜?”½", "ë³¸ì?€", 
-# #                   "ê²ƒìœ¼ë¡?", "ê·¸ëŸ¬?‚˜", "ê·¸ëŠ”", "ê²ƒì?€", "?œ„?•œ", "?•˜?Š”",
-# #                   "ê²ƒì´", "?†µ?•´", "?“±?„", "?´ë¥?", "?—†?Š”", "?‚˜?Š”"),
-# #   valuetype = "fixed")  
-# # 
-# print(toks[2], max_ndoc = 1, max_ntok = -1)
-# 
-# toks
-#   
-# # # below: the much slower version of cleaning the text
-# # data_chos <- data %>% 
-# #   filter(Newspaper == "Chosun") %>% 
-# #   mutate(Body = gsub("<.*?>", " ", Body, perl = TRUE),           #remove html 
-# #          gsub("[^?„±-?…|?…-?…£|ê°€-?£]", " ", Body, perl = TRUE),   #keep only Hangul
-# #          gsub("....ê¸°ì|....?Š¹?ŒŒ?›", " ", Body, perl = TRUE),    #remove author name, title
-# #          gsub("ì¢…ì´?‹ ë¬¸ë³´ê¸?", " ", Body, perl = TRUE)) %>%       #remove link descriptions
-# #   stringr::str_squish()
-# # 
-# # data_hani <- data %>% 
-# #   filter(Newspaper == "Hankyoreh") %>%  
-# #   mutate(Body = gsub("<.*?>", " ", Body, perl = TRUE), 
-# #          gsub("[^?„±-?…|?…-?…£|ê°€-?£]", " ", Body, perl = TRUE),  
-# #          gsub("....ê¸°ì|....?Š¹?ŒŒ?›", " ", Body, perl = TRUE)) %>%
-# #   stringr::str_squish()
-# # 
-# # data_hank <- data %>% 
-# #   filter(Newspaper == "Hankook") %>%  
-# #   mutate(Body = gsub("<.*?>", " ", Body, perl = TRUE), 
-# #          gsub("[^?„±-?…|?…-?…£|ê°€-?£]", " ", Body, perl = TRUE),  
-# #          gsub("....ê¸°ì|....?Š¹?ŒŒ?›", " ", Body, perl = TRUE), 
-# #          gsub("ì¢…ì´?‹ ë¬¸ë³´ê¸?", " ", Body, perl = TRUE),
-# #          gsub("?•œêµ??•„?´?‹·ì»? ë¬´ë‹¨? „?¬ ë°? ?¬ë°°í¬ ê¸ˆì?€|?¸?„°?„·?•œêµ??¼ë³? ë¬´ë‹¨ ? „?¬ ë°? ?¬ë°°í¬ ê¸ˆì?€|
-# #          ?•œêµ??•„?´?‹·ì»?|?•œêµ??˜¨?¼?¸?‹ ë¬¸í˜‘?šŒ|?””ì§€?„¸?‰´?Š¤?´?š©ê·œì¹™?— ?”°ë¥? ??€?‘ê¶Œì„ ?–‰?‚¬?•©?‹ˆ?‹¤\r|ì§€ë©´PDFë³´ê¸°",
-# #               "", Body, perl = TRUE)) %>%
-# #   stringr::str_squish()
-# 
-# setwd("C:/Users/hanou/Dropbox/RESEARCH/klarahan.github.io")
-# 
-# data_toks_wel <- toks %>% tokens_subset(Keyword == "welfare")
-# data_toks_uni <- toks %>% tokens_subset(Keyword == "unification")
-# data_toks_econ <- toks %>% tokens_subset(Keyword == "econdem")
-# data_toks_ideo <- toks %>% tokens_subset(Keyword %in% c("progressive", "conservative", "left", "right"))
-# data_toks_dem <- toks %>% tokens_subset(Keyword == "democracy")
-# 
-# saveRDS(toks, "data_toks.RDS")
-# saveRDS(data_toks_wel, "data/data_toks_wel.RDS")
-# saveRDS(data_toks_uni, "data/data_toks_uni.RDS")
-# saveRDS(data_toks_econ, "data/data_toks_econ.RDS")
-# saveRDS(data_toks_ideo, "data/data_toks_ideo.RDS")
-# saveRDS(data_toks_dem, "data/data_toks_dem.RDS")
-# 
-# data_dfm_wel <- data_toks_wel %>% dfm() 
-# data_dfm_uni <- data_toks_uni %>% dfm()
-# data_dfm_econ <- data_toks_econ %>% dfm()
-# data_dfm_ideo <- data_toks_ideo %>% dfm()
-# data_dfm_dem <- data_toks_dem %>% dfm()
-# 
-# saveRDS(data_dfm_wel, "data/data_dfm_wel.RDS")
-# saveRDS(data_dfm_uni, "data/data_dfm_uni.RDS")
-# saveRDS(data_dfm_econ, "data/data_dfm_econ.RDS")
-# saveRDS(data_dfm_ideo, "data/data_dfm_ideo.RDS")
-# saveRDS(data_dfm_dem, "data/data_dfm_dem.RDS")
-
